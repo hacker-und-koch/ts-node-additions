28de58946d6fef2f259043afddae9c750541f9d8
Sam Di Velopa <punkt.punktdot.dot@gmail.com>
2020-03-22 14:53:27 +0100

    deployment: fix release commit message

--- .run.js
+++ .run.js
@@ -95,7 +95,7 @@ async function run(argv, debug) {
 
         console.log('++ commiting and pushing changes');
         await git(['add', './package.json', ...WORKSPACES.map(ws => `${ws}/package.json`)]);
-        await git(['commit', '-m', `release: ${VERSION}`]);
+        await git(['commit', '-m', `'release: ${VERSION}'`]);
         await git(['tag', `release-${VERSION}`]);
         await git(['push', '--tags']);
 
