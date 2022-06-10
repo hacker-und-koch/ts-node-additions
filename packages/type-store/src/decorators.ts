13571a585a055cdb4284bbd01a0470067a9be406
Sam Di Velopa <punkt.punktdot.dot@gmail.com>
2022-06-10 15:10:10 +0200

    feat(type-store): handle mandatory and optional keys

--- packages/type-store/src/decorators.ts
+++ packages/type-store/src/decorators.ts
@@ -1,41 +1,119 @@
 import "reflect-metadata";
 
-export const INTERNAL_TYPE_KEY = Symbol('__et_type_info__');
-export const INTERNAL_NAME_KEY = Symbol('__et_name__');
+export const INTERNAL_TYPE_KEY = Symbol('__ts_type_info__');
+export const INTERNAL_NAME_KEY = Symbol('__ts_name__');
+export const INTERNAL_VALUE_DB = Symbol('__ts_value_db__');
 
-export function DataType(): any {
-    return function (
-        target: any,
-    ): any {
-        target[INTERNAL_NAME_KEY] = target.name;
+export interface DataTypeDecorated {
+    [INTERNAL_NAME_KEY]: string;
+    // create(template: any): T & DataTypeDecorated {
+
+    // };
+}
+
+export interface ValueOptions {
+    default: any;
+}
+
+export function DataType() {
+    return function <T /* extends new (...args: any[]) => T & DataTypeDecorated */>(
+        target: T
+    ): DataTypeDecorated & T {
+        // (target as any).__proto__.constructor = (template: any) => {
+        //     for (let key in template) {
+        //         this[key] = template[key];
+        //     }
+        // }
+        (target as any).__proto__.constructor = () => {
+            return 'yoyo'
+        }
+        return Object.assign(target as T, {
+            [INTERNAL_NAME_KEY]: (target as any).name,
+            toString() { return 'yayo' }
+            // CANNOT BE TYPED:
+            // create: (template?: T) => {
+            //     const instance: T = new (target as any)();
+            //     if (template !== undefined) {
+            //         for (let key in template) {
+            //             if (!(key in (instance as any).__proto__.constructor[INTERNAL_TYPE_KEY])) {
+            //                 throw new Error(`Type template contained unknown key '${key}'`);
+            //             }
+
+
+            //             instance [key] = template[key];
+            //             console.log(instance);
+            //         }
+
+            //     }
+            //     console.log('instance:', instance);
+            //     return instance;
+            // }
+        }) as any;
     }
 }
 
-export function Optional(): any {
+export function Optional(opts?: ValueOptions): any {
     return function (
         target: any,
         propertyKey: string | symbol,
     ): any {
         const ownType = Reflect.getMetadata("design:type", target, propertyKey);
-      
+
         if (!target.constructor[INTERNAL_TYPE_KEY]) {
             target.constructor[INTERNAL_TYPE_KEY] = {};
         }
+
+        if (!target[INTERNAL_VALUE_DB]) {
+            Object.defineProperty(target, INTERNAL_VALUE_DB, {
+                value: {},
+                enumerable: false,
+            });
+        }
+
+        Object.defineProperties((target as any), {
+            [propertyKey]: {
+                get: () => target[INTERNAL_VALUE_DB][propertyKey],
+                set: (x: any) => {
+                    if (!checkType(x, ownType)) {
+                        throw new Error('Invalid assignment to DataType');
+                    }
+                    target[INTERNAL_VALUE_DB][propertyKey] = x;
+                },
+                configurable: false,
+                enumerable: true,
+            }
+        });
+
+        if (opts?.default !== undefined) {
+            // may want to check typeof default..
+            target[INTERNAL_VALUE_DB][propertyKey] = opts.default;
+        }
+
         target.constructor[INTERNAL_TYPE_KEY][propertyKey] = {
             type: ownType,
         }
+
+        return target;
     }
 }
 
+function checkType(value: any, template: any): boolean {
+    const has = typeof value;
+    const wants = template.name.toLowerCase();
 
-export function Mandatory(): any {
+    return has === wants;
+}
+
+
+export function Mandatory(opts?: ValueOptions): any {
     return function (
         target: any,
         propertyKey: string | symbol,
         descriptor: PropertyDescriptor,
 
     ): any {
-        Optional()(target, propertyKey, descriptor);
+        Optional(opts)(target, propertyKey, descriptor);
         target.constructor[INTERNAL_TYPE_KEY][propertyKey].mandatory = true;
+        return target;
     }
 }
--- packages/type-store/src/example.ts
+++ packages/type-store/src/example.ts
@@ -1,3 +1,4 @@
+import { DataTypeDecorated } from './decorators';
 import {
     DataType,
     Mandatory,
@@ -19,56 +20,64 @@ class Foo {
 class Bar {
     @Mandatory()
     someKey: Foo;
+    @Optional({default: BigInt(6)})
+    id?: bigint;
     @Optional()
-    id: bigint;
-    @Optional()
-    name: string;
+    name?: string;
 }
+
+const bar: Bar = new Bar();
+bar.id = BigInt(5);
+
+bar.name = "String";
+bar['name'] = JSON.parse('6');  //fails on purpose
+
+console.log('#########', bar, bar.id, bar.name, '###########');
 
 const store = new TypeStore();
 
 store.add(Foo);
 store.add(Bar);
 
-console.log('TEMPLATES >>>', store.storage);
+// console.log('TEMPLATES >>>', store.storage);
 
-console.log('RESULT 1 >>>', store.check({
-    id: 5,
-    text: 'Lorem Ipsum'
-}, Foo));
+// console.log('RESULT 1 >>>', store.check({
+//     id: 5,
+//     text: 'Lorem Ipsum'
+// }, Foo));
 
-console.log('RESULT 2 >>>', store.check({
-    id: '5',
-    text: 'Lorem Ipsum'
-}, Foo));
+// console.log('RESULT 2 >>>', store.check({
+//     id: '5',
+//     text: 'Lorem Ipsum'
+// }, Foo));
 
-console.log('RESULT 3 >>>', store.check({
-    text: ''
-}, Foo));
+// console.log('RESULT 3 >>>', store.check({
+//     text: ''
+// }, Foo));
 
-console.log('RESULT 4 >>>', store.check({
-    id: 5,
-    extra: 'key',
-    text: 'Lorem Ipsum'
-}, Foo));
+// console.log('RESULT 4 >>>', store.check({
+//     id: 5,
+//     extra: 'key',
+//     text: 'Lorem Ipsum'
+// }, Foo));
 
 
-console.log('RESULT 5 >>>', store.check({
-    someKey: {
-        id: 1,
-        text: 'sid amet',
-    },
-    id: BigInt(200),
-}, Bar));
+// console.log('RESULT 5 >>>', store.check({
+//     someKey: {
+//         id: 1,
+//         text: 'sid amet',
+//     },
+//     id: BigInt(200),
+// }, Bar));
 
-console.log('RESULT 6 >>>', store.check({
-    someKey: {
-        id: 'nope',
-        text: 5,
-    },
-    id: BigInt(200),
-}, Bar));
+// console.log('RESULT 6 >>>', store.check({
+//     someKey: {
+//         id: 'nope',
+//         text: 5,
+//     },
+//     id: BigInt(200),
+// }, Bar));
 
-console.log('RESULT 7 >>>', store.check({}, Bar));
+// console.log('RESULT 7 >>>', store.check({}, Bar));
 
-console.log('SCHEMAS >>>', JSON.stringify(store.schemas, null, 2));
+// console.log('SCHEMAS >>>', JSON.stringify(store.schemas, null, 2));
